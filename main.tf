0774eacc7173709bbbb551df7d1a311e64a4856b
donaldcarol <donald@dakini.network>
2026-02-10 16:49:09 +0200

    Initial commit (clean)

---
+++ main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-terraform-lab-01"
+  location = "westeurope"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-lab-01"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.10.0.0/16", "10.20.0.0/16"]
+}
+resource "azurerm_subnet" "snet_10_1" {
+  name                 = "snet-10-1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.10.1.0/24"]
+}
+
+
+resource "azurerm_subnet" "snet_20_1" {
+  name                 = "snet-20-1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.20.1.0/24"]
+}
